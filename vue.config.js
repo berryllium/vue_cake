9d6643bc4785a4b17f40ca6fdd7284ca61cebb0c
berryllium <gorkundp@yandex.ru>
2019-11-12 14:04:35 +0300

    работает из корневой директории сайта

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/tmp/cake/'
-    : '/'
-}
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/tmp/cake/'
+//     : '/'
+// }
