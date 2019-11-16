c6d16d6fe7af61148fd75a741471f6cfa3880b4f
berryllium <gorkundp@yandex.ru>
2019-11-16 14:54:51 +0300

    new password

--- public/admin/connection.php
+++ public/admin/connection.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', 'localhost');
-define('DB', 'lesson5');
-define('LOGIN', 'root');
-define('PASSWORD', '');
+define('DB', 'u435826_tortik');
+define('LOGIN', 'u435826_tortik');
+define('PASSWORD', '0V9u5A9k');
 
 $connection = mysqli_connect(HOST,LOGIN,PASSWORD,DB) or die ('Ошибка подлючения к БД');
